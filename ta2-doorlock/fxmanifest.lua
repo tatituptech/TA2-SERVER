74f5250660a67d23ad07652821897de3d8876c07
TATITUPTECH <ratchetboi5150@gmail.com>
2026-02-01 12:23:40 -0500

    Update author in fxmanifest.lua

--- ta2-doorlock/fxmanifest.lua
+++ ta2-doorlock/fxmanifest.lua
@@ -1,7 +1,7 @@
 ﻿fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'TATITUPTECH'
 description 'Door management system allowing players to lock and unlock doors'
 version '2.0.0'
 
