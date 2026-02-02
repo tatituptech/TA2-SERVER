6ff1f6d8a9f010145d9f9c4023d741dab85d9992
TATITUPTECH <ratchetboi5150@gmail.com>
2026-02-02 11:18:49 -0500

    Update fxmanifest.lua

--- ta2-vehiclesales/fxmanifest.lua
+++ ta2-vehiclesales/fxmanifest.lua
@@ -1,7 +1,7 @@
 ﻿fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'TATITUPTECH'
 description 'Allows players to set their vehicles on display for sale to other players'
 version '1.2.0'
 
@@ -34,3 +34,4 @@ files {
     'html/vue.min.js',
     'html/ui.js',
 }
+
