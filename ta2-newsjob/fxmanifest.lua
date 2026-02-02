e2b9772b2d6916e5ed253e3fdc722cb026ed3f88
TATITUPTECH <ratchetboi5150@gmail.com>
2026-02-02 09:02:21 -0500

    Update fxmanifest.lua

--- ta2-newsjob/fxmanifest.lua
+++ ta2-newsjob/fxmanifest.lua
@@ -1,7 +1,7 @@
 ﻿fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'TATITUPTECH'
 description 'Allows players to play as a news reporter and access the equipment for it'
 version '1.3.0'
 
