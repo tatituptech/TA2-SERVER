38a23c15e9aa2f856f29aaf93246c0fae1d5d109
TATITUPTECH <ratchetboi5150@gmail.com>
2026-04-13 23:11:09 -0400

    Update author name in fxmanifest.lua

--- ta2-scoreboard/fxmanifest.lua
+++ ta2-scoreboard/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'TATITUPTECH'
 description 'Allows players to view a menu that displays various information about the server and players'
 version '1.2.1'
 
