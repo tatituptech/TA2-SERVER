d821ec43833fdaa6f54d449c091d32c56bf55335
TATITUPTECH <ratchetboi5150@gmail.com>
2026-04-13 23:07:18 -0400

    Update author name in fxmanifest.lua

--- ta2-streetraces/fxmanifest.lua
+++ ta2-streetraces/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'TATITUPTECH'
 description 'Allows players to compete in quick vehicle races with wagers'
 version '1.3.0'
 
