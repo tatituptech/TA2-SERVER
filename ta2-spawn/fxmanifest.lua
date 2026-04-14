7c00644221dbb4e1d752a12943906fe093195fcf
TATITUPTECH <ratchetboi5150@gmail.com>
2026-04-13 23:08:06 -0400

    Update author name in fxmanifest.lua

--- ta2-spawn/fxmanifest.lua
+++ ta2-spawn/fxmanifest.lua
@@ -1,7 +1,7 @@
 ﻿fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'TATITUPTECH'
 description 'Allows players to select a spawn point from a list of available locations'
 version '1.2.0'
 
